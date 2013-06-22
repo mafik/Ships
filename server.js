4edc5a50b0cce4edb08a673a15c74ded5fb36fd4
Grzegorz Graczyk <grzesiu@Kardon.(none)>
2013-06-22 22:53:10 +0200

    Server.js

--- server.js
+++ server.js
@@ -11,15 +11,59 @@ app.listen(port);
 
 console.log('Listening on port ' + port);
 
+var treasures = {};
+var pirates = {};
+var corsairs = {};
+
+var players = {};
+
 io.sockets.on('connection', function (socket) {
     var player = undefined;
 	var address = socket.handshake.address;
 
 	console.log('connected ' + address);
 
-	
+	socket.on('hello', function(data) {
+		if( data in pirates ) {
+			socket.disconnect();
+			return;
+		}
+
+		player = data;
+
+		pirates[player] = {
+			id: player,
+			x: 50,
+			y: 50,
+			alpha: 0
+		};
+
+		
+		for( var key in pirates ) {
+			players[key].socket.emit('add_pirate', pirates[player] );
+		}
+
+		players[player] = {
+			address: address,
+			socket: socket
+		};
+	});
+
 	socket.on('message', function(data) {
 		var str = JSON.stringify(data);
 		console.log(str);
 	});
+
+	socket.on('disconnect', function(data) {
+		if( data in pirates ) {
+			delete pirates[player];
+		}
+
+		for( var key in pirates ) {
+			players[key].socket.emit('remove_pirate', player );
+		}
+	} );
+
+	
+
 });
