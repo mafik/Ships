c4b42478a2fa26fac9310cb91d6641fad54eeb5c
Ziober <ziobrowski.bartek@gmail.com>
2013-06-23 17:20:34 +0200

    nie dziala cos

--- static/game.js
+++ static/game.js
@@ -108,6 +108,8 @@ var get_image = function(url) {
 	return image_cache[url];
 };
 
+var previous_pirates = clone(game.corsairs);
+var previous_corsairs = clone(game.pirates);
 var circle = function(obj) {
 	ctx.save();
 	var move = (game.now - game.updatedAt)/16;
@@ -205,11 +207,11 @@ var another_pirate_Icon = function(obj){
 
 	ctx.restore();
 }
-var enemyIcon = function(obj){
+var enemyIcon = function(obj,pre){
 	ctx.save();	
 	var move = (game.now - game.updatedAt)/16;
 	ctx.translate(obj.x + move*obj.vx, obj.y + move*obj.vy);
-	ctx.rotate(Math.atan2(me.x,me.y));
+	ctx.rotate(Math.atan2(obj.x-pre.x,obj.y-pre.y));
 	var lineWidth = 2;
 	var canvasWidth = 30;
 	var canvasHeight = 1.15*canvasWidth;
@@ -458,6 +460,8 @@ var draw_pirate = function(obj) {
 }
 var draw_world = function() {
 
+
+	
 	ctx.fillStyle = 'rgb(255, 255, 0)';
 	for(var key in game.treasures) {
 		treasure_icon(game.treasures[key]);
@@ -465,20 +469,23 @@ var draw_world = function() {
 
 	ctx.fillStyle = 'rgb(255, 0, 0)';
 	for(var key in game.corsairs) {
-		enemyIcon(game.corsairs[key]);
+		enemyIcon(game.corsairs[key],previous_corsairs[key]);
 	}
 
 	ctx.fillStyle = 'rgb(0, 155, 0)';
 	for(var key in game.pirates) {
 		if(game.pirates[key]!=me)
-		another_pirate_Icon(game.pirates[key]);
+		another_pirate_Icon(game.pirates[key],previous_pirates[key]);
 	}
 
 	ctx.fillStyle = 'rgb(12, 155, 0)';
 	if(me){
 		draw_pirate(me);
 	}
-};
+	previous_corsairs = clone(game.corsairs);
+	previous_pirates = clone(game.pirates);
+	
+}
 
 var camera = {x: 0, y: 0};
 camera.update = function(dt) {
